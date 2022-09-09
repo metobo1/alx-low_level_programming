e0f029840bfdc568b4b5cb22223dca10ef552974
root <frankgichaba@gmail.com>
2022-09-09 15:18:49 -0700

    worked

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <studio.h>
+/* This program will assign a random number to the variable n each time */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = i;
+	/* your code goes there */
+	return (0);
+}
